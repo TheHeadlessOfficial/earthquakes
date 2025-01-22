76d10e83016ae3355f3d52787a7fbb72b09acf03
T-H <194772282+TheHeadlessOfficial@users.noreply.github.com>
2025-01-22 09:38:53 +0100

    earthquakes

---
+++ .conky/earthquakes/earthquakes.py
@@ -0,0 +1,104 @@
+import os, sys
+import requests
+# Lock file to tell conky that the script is running
+lock_file = "/tmp/script_done.lock"
+# Crea il file di lock all'inizio
+try:
+    open(lock_file, 'w').close()
+    #   number of rows
+    rows = 30
+    ################################ get your HOME name automatically
+    homepath = os.environ['HOME']
+    homename = homepath
+    homename = homename[6:]
+    ###################################                  website url
+    url = "https://www.seismicportal.eu/fdsnws/event/1/query?callback=angular.callbacks._0&format=jsonp&limit=" + str(rows) + "&offset=1&orderby=time"
+    res = requests.get(url)
+    data = res
+    ###################################                  file paths
+    home = '/home/'
+    conkypath = "/.conky/"
+    filepathtxt = home + homename + conkypath + 'earthquakes/eq.txt'
+    #filepathjson = '/home/hiro/.conky/earthquakes/eq.json'
+    filepathconky = home + homename + conkypath + 'earthquakes/eqconky.txt'
+    filepathconkyfin = home + homename + conkypath + 'earthquakes/eqconkyfin.txt'
+    ###################################                  get the HTML page source code in a txt file named eq.txt
+    with open(filepathtxt, 'w') as f:
+        f.write(data.text)
+    # first get all lines from file
+    with open(filepathtxt, 'r') as f:
+        lines = f.readlines()
+    # # remove spaces
+    # lines = [line.replace(' ', '') for line in lines]
+    # remove comma
+    lines = [line.replace(',', '') for line in lines]
+    # remove quotes
+    lines = [line.replace('"', '') for line in lines]
+    # finally, write lines in the file
+    with open(filepathtxt, 'w') as f:
+        f.writelines(lines)
+    ###################################                  write conky statements
+    #   mag color
+    maggreen = '${color6}'
+    magorange = '${color3}'
+    magred = '${color4}'
+    j = 24
+    delta = 0
+    time = 600
+    with open(filepathconky, 'w') as fo:
+        for i in range(0, rows):
+            temp = j + delta - 1
+            #   select mag color
+            fomag = open(filepathtxt, 'r')
+            eqtxt = fomag.readlines()
+            string = (eqtxt[temp - 1])
+            rest, n = string.rsplit(':', 1)
+            value = float(n)
+            fomag.close()
+            if (value >= 0 and value < 4):
+                row1_1 = maggreen + "${execpi " + str(time) + " sed -n '" + str(temp + 1) + "p' $HOME" + conkypath + "earthquakes/eq.txt | cut -c14-16} "
+                row1_2 = maggreen + "${execpi " + str(time) + " sed -n '" + str(temp) + "p' $HOME" + conkypath + "earthquakes/eq.txt | cut -c10-12}${color}"
+            elif (value >= 4 and value < 6):
+                row1_1 = magorange + "${execpi " + str(time) + " sed -n '" + str(temp + 1) + "p' $HOME" + conkypath + "earthquakes/eq.txt | cut -c14-16} "
+                row1_2 = magorange + "${execpi " + str(time) + " sed -n '" + str(temp) + "p' $HOME" + conkypath + "earthquakes/eq.txt | cut -c10-12}${color}"
+            elif (value >= 6):
+                row1_1 = magred + "${execpi " + str(time) + " sed -n '" + str(temp + 1) + "p' $HOME" + conkypath + "earthquakes/eq.txt | cut -c14-16} "
+                row1_2 = magred + "${execpi " + str(time) + " sed -n '" + str(temp) + "p' $HOME" + conkypath + "earthquakes/eq.txt | cut -c10-12}${color}"
+            fo.write('{}\n'.format(row1_1))
+            fo.write('{}\n'.format(row1_2))
+            #   end mag color
+            temp = j + delta - 8
+            row1_3 = "${GOTO 50}${execpi " + str(time) + " sed -n '" + str(temp) + "p' $HOME" + conkypath + "earthquakes/eq.txt | cut -c11-20}"
+            fo.write('{}\n'.format(row1_3))
+            temp = j + delta - 6
+            row1_4 = "${GOTO 120}${execpi " + str(time) + " sed -n '" + str(temp) + "p' $HOME" + conkypath + "earthquakes/eq.txt | cut -c10-14}"
+            fo.write('{}\n'.format(row1_4))
+            temp = j + delta - 5
+            row1_5 = "${GOTO 160}${execpi " + str(time) + " sed -n '" + str(temp) + "p' $HOME" + conkypath + "earthquakes/eq.txt | cut -c10-14}"
+            fo.write('{}\n'.format(row1_5))
+            temp = j + delta - 4
+            row1_6 = "${GOTO 210}${execpi " + str(time) + " sed -n '" + str(temp) + "p' $HOME" + conkypath + "earthquakes/eq.txt | cut -c12-16}"
+            fo.write('{}\n'.format(row1_6))
+            temp = j + delta - 2 
+            row1_7 = "${GOTO 265}${execpi " + str(time) + " sed -n '" + str(temp) + "p' $HOME" + conkypath + "earthquakes/eq.txt | cut -c11-16}"
+            fo.write('{}\n'.format(row1_7))
+            temp = j + delta - 7
+            row1_8 = "${GOTO 310}${execpi " + str(time) + " sed -n '" + str(temp) + "p' $HOME" + conkypath + "earthquakes/eq.txt | cut -c19-50}"
+            fo.write('{}\n'.format(row1_8))
+            delta = delta + 26
+    #fo.close()
+    ###################################                  write conky final statements to copy in conky file
+    last = rows * 8 + 1
+    with open(filepathconkyfin, 'w') as fo:
+        for i in range(1, last):
+            temp = i
+            rowconky = "${execpi " + str(time) + " sed -n '" + str(temp) + "p' $HOME" + conkypath + "earthquakes/eqconky.txt}"
+            fo.write('{}\n'.format(rowconky))
+except Exception as e:
+    # Gestione delle eccezioni (opzionale)
+    filelockerror = (f"Error during script execution: {e}")
+    #print(filelockerror)
+finally:
+    # Rimuovi il file di lock alla fine, anche in caso di errore
+    os.remove(lock_file)
+    #print("File di lock rimosso.")
---
+++ README.md
@@ -0,0 +1,36 @@
+# Conky Moon Phases
+ 
+A standalone conky (written in Python) that shows the moon phases, using [moongiant.com](https://www.moongiant.com/) website.<br>
+Based on https://gitlab.com/teobigusgeekus/Moongiant_conky_script<br>
+<br>
+<br>
+
+## **WIKI**<br>
+
+Download the .zip file, extract the files, copy the file `.conkyrc_moonpy` and the folder `.conky` inside your Linux `home`.<br>
+If your `home` is named *pippo*, copy inside *pippo* so you get: `/home/pippo/.conky` and `/home/pippo/.conkyrc_moonpy`<br>
+Go to `/home/YOURHOMENAME/.conky/moon/Moongiant_python/font` and install the font.<br>
+Go to `/home/YOURHOMENAME/.conky/moon/Moongiant_python` and open with a text editor the file `moongiantpy.py`, go to line 17 and edit your emisphere (north is default).<br>
+<br>
+The python script saves data in files so you can build your conky moon as you wish.<br>
+The `.conkyrc_moonpy` file i attach, works.<br>
+Run the file `.conkyrc_moonpy` from terminal (the first time you run this conky), so you can get possible errors. 
+
+
+
+
+<br>
+<br>
+
+## Screenshot
+
+![](https://github.com/TheHeadlessOfficial/moon-phases/blob/main/.conky/moon/Moongiant_python/Moongiant_README-INFO/screenshot_01.png)<br>
+
+<br>
+<br>
+<br>
+<br>
+<br>
+
+---
+[Markdown guide](https://docs.github.com/en/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax)
